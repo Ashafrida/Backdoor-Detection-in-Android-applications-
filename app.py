659e4c24e160541a5c462a165e33840a63b48020
d-een Bashir <mohameddinhbashir@gmail.com>
2023-04-03 09:33:47 +0300

    update app.py

--- app.py
+++ app.py
@@ -6,18 +6,22 @@ from apscheduler.triggers.interval import IntervalTrigger
 import time
 import os
 
-network_traffics="./TrainedModel/"
-permission="./gnb_model_permissions.pkl"
+network_traffics="./DATASET/network_traffics.csv"
+permission="./TrainedModel/gnb_model_permissions.pkl"
+train_randomforest= "./train_randomforest.py"
+train_keras="./train_keras.py"
+train_svm="./train_svm"
+
 schedular= BlockingScheduler
 
 
 def home():
-    print("Welcome")
+    print("---Android Backdoor Detection---")
     
 @schedular.scheduled_job(IntervalTrigger(hours=3))
 def permission():
     return permissions 
-
+schedular.start()
 
 @schedular.scheduled_job(IntervalTrigger(hours=3))
 def traffics():
